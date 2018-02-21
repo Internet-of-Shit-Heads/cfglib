c9803de5eb860cadc35eefd24b72c768e86d6c1d
Mino Sharkhawy <mino.sharkhawy@student.tuwien.ac.at>
2018-02-21 04:44:08 +0100

    Initial version of cfglib

---
+++ cfglib.h
@@ -0,0 +1,27 @@
+#ifndef __CFGLIB_H__
+#define __CFGLIB_H__
+
+struct sensor_info {
+	uint16_t id;
+	at_ac_tuwien_iot1718_N2C_SensorType type;
+	uint8_t pin;
+	uint8_t degree;
+	const float *poly;
+} __attribute__((packed));
+
+struct rfnode_config {
+	uint64_t address;
+	uint32_t room_number;
+	uint32_t node_id;
+	unsigned int wakeup_interval;
+	unsigned int baud_rate;
+	uint16_t cepin;
+	uint16_t cspin;
+	uint8_t channel;
+	uint8_t delay;
+	uint8_t retransmits;
+	const uint8_t *auth_key;
+	const struct sensor_info *const *sensors; 
+} __attribute__((packed));
+
+#endif /* __CFGLIB_H__ */
