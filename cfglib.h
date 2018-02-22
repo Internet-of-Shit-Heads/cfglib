01b9163dfc986d8b24f1c939ded3c08c5b857ecf
Mino Sharkhawy <mino.sharkhawy@student.tuwien.ac.at>
2018-02-22 17:07:16 +0100

    Add debugging facilities to cfglib

--- cfglib.h
+++ cfglib.h
@@ -11,19 +11,66 @@ struct sensor_info {
 	const float *poly;
 } __attribute__((packed));
 
+enum debug_level {
+	DEBUG_OFF,
+	DEBUG_ERRORS,
+	DEBUG_NOTICE,
+	DEBUG_ALL
+};
+
 struct rfnode_config {
-	uint64_t address;
+	/* operations */
+	const struct sensor_info *const *sensors; 
 	uint32_t room_number;
 	uint32_t node_id;
 	unsigned int wakeup_interval;
-	unsigned int baud_rate;
-	uint16_t cepin;
-	uint16_t cspin;
+	/* communications */
+	uint64_t address;
 	uint8_t channel;
 	uint8_t delay;
 	uint8_t retransmits;
+	/* security */
 	const uint8_t auth_key[CRYPTLIB_KEY_SIZE];
-	const struct sensor_info *const *sensors; 
+	/* hardware */
+	uint16_t cepin;
+	uint16_t cspin;
+	uint16_t rtcpin;
+	uint16_t rtcint;
+	/* debug */
+	unsigned int baud_rate;
+	debug_level debug;
 } __attribute__((packed));
+
+struct coordinator_config {
+	/* operations */
+	/* communications */
+	uint64_t address;
+	uint8_t channel;
+	uint8_t delay;
+	uint8_t retransmits;
+	/* security */
+	const uint8_t auth_key[CRYPTLIB_KEY_SIZE];
+	/* hardware */
+	uint16_t cepin;
+	uint16_t cspin;
+	/* debug */
+	unsigned int baud_rate;
+	debug_level debug;
+} __attribute__((packed));
+
+#ifdef COORDINATOR
+	extern struct coordinator_config current_cfg;
+#else
+	extern const struct sensor_info *const available_sensors[];
+#	define N_SENSORS (sizeof(available_sensors) / sizeof(struct sensor_info *))
+
+	extern struct rfnode_config current_cfg;
+#endif
+
+#ifdef DEBUG_HARD_OFF
+#	define DO_DEBUG(lvl)	if (0)
+#else
+#	define DO_DEBUG(lvl)	if (current_cfg.debug >= (lvl))
+#endif
 
 #endif /* __CFGLIB_H__ */
